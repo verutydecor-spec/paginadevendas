66f8f2880176af7d21ac9ea6817e8fa08057124c
verutydecor-spec <verutydecor@gmail.com>
2025-08-12 17:46:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
+// vite.config.js
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react()],
+    base: '/paginadevendas/', // 👈 nome exato do seu repositório no GitHub Pages
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    build: {
+      outDir: 'dist',
+    }
+  };
 });
